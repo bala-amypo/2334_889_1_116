47362044a5335a4c57b9ec6ea94c59b2111b7b49
bala-amypo <bala@amypo.in>
2025-12-18 07:37:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/ContractController.java
+++ src/main/java/com/example/demo/controller/ContractController.java
@@ -3,12 +3,8 @@ package com.example.project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import com.example.project.entity.Contract;
 import com.example.project.service.ContractServices;
 
@@ -19,18 +15,34 @@ public class ContractController {
     @Autowired
     private ContractServices service;
 
+    // CREATE
     @PostMapping("/post")
     public Contract postData(@RequestBody Contract ct) {
         return service.saveData(ct);
     }
 
+    // READ ALL
     @GetMapping("/get")
     public List<Contract> getAllData() {
         return service.getAllData();
     }
 
+    // READ BY ID
     @GetMapping("/get/{id}")
     public Contract getById(@PathVariable Long id) {
         return service.getById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/update/{id}")
+    public Contract updateData(@PathVariable Long id, @RequestBody Contract ct) {
+        return service.updateData(id, ct);
+    }
+
+    // DELETE
+    @DeleteMapping("/delete/{id}")
+    public String deleteData(@PathVariable Long id) {
+        service.deleteData(id);
+        return "Contract with ID " + id + " deleted successfully!";
     }
 }
--- src/main/java/com/example/demo/service/ContractServices.java
+++ src/main/java/com/example/demo/service/ContractServices.java
@@ -3,7 +3,6 @@ package com.example.project.service;
 import java.util.*;
 
 import org.springframework.stereotype.Service;
-
 import com.example.project.entity.Contract;
 
 @Service
@@ -11,24 +10,29 @@ public class ContractServices {
 
     private Map<Long, Contract> mp = new HashMap<>();
 
+    // CREATE
     public Contract saveData(Contract ct) {
         mp.put(ct.getId(), ct);
         return ct;
     }
 
+    // READ ALL
     public List<Contract> getAllData() {
         return new ArrayList<>(mp.values());
     }
 
+    // READ BY ID
     public Contract getById(Long id) {
         return mp.get(id);
     }
 
+    // UPDATE
     public Contract updateData(Long id, Contract data) {
         mp.put(id, data);
         return data;
     }
 
+    // DELETE
     public void deleteData(Long id) {
         mp.remove(id);
     }
