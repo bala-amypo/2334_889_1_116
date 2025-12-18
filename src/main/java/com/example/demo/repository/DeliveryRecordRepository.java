6556f6d8ff3b7798db8976f8ba7d54469fbaf2c5
bala-amypo <bala@amypo.in>
2025-12-18 17:07:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
+++ src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
@@ -1,10 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.project.entity.DeliveryRecord;
+import com.example.demo.entity.DeliveryRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeliveryRecordRepository extends JpaRepository<DeliveryRecord, Long> {
-    // You can add custom queries if needed
 }
--- src/main/java/com/example/demo/repository/PenaltyCalculationRepository.java
+++ src/main/java/com/example/demo/repository/PenaltyCalculationRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.project.entity.PenaltyCalculation;
+import com.example.demo.entity.PenaltyCalculation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
