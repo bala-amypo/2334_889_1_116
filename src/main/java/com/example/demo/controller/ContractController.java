1e8a687074fc95c97ade3477e9a8e99cec7f57b9
bala-amypo <bala@amypo.in>
2025-12-18 07:45:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/ContractController.java
+++ src/main/java/com/example/demo/controller/ContractController.java
@@ -14,32 +14,25 @@ public class ContractController {
 
     @Autowired
     private ContractServices service;
-
-    // CREATE
     @PostMapping("/post")
     public Contract postData(@RequestBody Contract ct) {
         return service.saveData(ct);
     }
-
-    // READ ALL
     @GetMapping("/get")
     public List<Contract> getAllData() {
         return service.getAllData();
     }
 
-    // READ BY ID
     @GetMapping("/get/{id}")
     public Contract getById(@PathVariable Long id) {
         return service.getById(id);
     }
 
-    // UPDATE
     @PutMapping("/update/{id}")
     public Contract updateData(@PathVariable Long id, @RequestBody Contract ct) {
         return service.updateData(id, ct);
     }
 
-    // DELETE
     @DeleteMapping("/delete/{id}")
     public String deleteData(@PathVariable Long id) {
         service.deleteData(id);
