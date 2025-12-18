2999361abf704f080a61367c48f67f03a718d3fb
bala-amypo <bala@amypo.in>
2025-12-18 15:21:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/BreachReport.java
+++ src/main/java/com/example/demo/entity/BreachReport.java
@@ -12,7 +12,29 @@ public class BreacchReport{
     public BreachReport(Long id,int daysDelayed,String remark){
         this.id=id;
         this.daysDelayed=daysDelayed;
-        
+        this.remark=remark;
+    }
+    public Long getId() {
+        return id;
     }
 
+    public int getDaysDelayed() {
+        return daysDelayed;
+    }
+
+    public String remark() {
+        return remark;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDaysDelayed(String daysDelayed) {
+        this.daysDelayed = daysDelayed;
+    }
+
+    public void remark(String remark) {
+        this.remark = remark;
+    }
 }
