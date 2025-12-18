0ef63f9010e6c478035bbca3ac99dc0ca9a6a04d
bala-amypo <bala@amypo.in>
2025-12-18 08:25:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/BreachReport.java
+++ src/main/java/com/example/demo/entity/BreachReport.java
@@ -2,6 +2,6 @@ package com.example.demo.entity;
 public class BreacchReport{
     private Long id;
     private int daysDelayed;
-    private Long pen;
-    private Long id;
+    private BigDecimal penaltyAmount;
+    private String remark;
 }
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,6 @@
+package com.example.demo.entity;
+public class User{
+    private Long id;
+    private String email;
+    private String pa;
+}
