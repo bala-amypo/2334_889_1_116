39a4a84bab82f4f0685b2613f18fd15566a3ed71
bala-amypo <bala@amypo.in>
2025-12-18 15:06:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/BreachReportController.java
+++ src/main/java/com/example/demo/controller/BreachReportController.java
@@ -1,5 +1,14 @@
+package com.example.demo.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.BreachReport;
+import com.example.demo.service.BreachReportService;
 
+@RestController
+@RequestMapping("/BreachReport")
 public class BreachReportController {
-
+    @Autowired
+    private
 }
