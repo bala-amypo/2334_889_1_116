a77f71ad311a7271b17d3e15179e87baf6b6f2cd
bala-amypo <bala@amypo.in>
2025-12-23 07:57:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -1,47 +1,48 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import jakarta.persistence.*;
+// import jakarta.persistence.*;
 
-@Entity
-public class Contract {
+// @Entity
+// public class Contract {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(unique = true)
-    private String contractNumber;
-    private String title;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     @Column(unique = true)
+//     private String contractNumber;
+//     private String title;
 
-    public Contract() {
-    }
+//     public Contract() {
+//     }
 
-    public Contract(Long id, String contractNumber, String title) {
-        this.id = id;
-        this.contractNumber = contractNumber;
-        this.title = title;
-    }
+//     public Contract(Long id, String contractNumber, String title) {
+//         this.id = id;
+//         this.contractNumber = contractNumber;
+//         this.title = title;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public String getContractNumber() {
-        return contractNumber;
-    }
+//     public String getContractNumber() {
+//         return contractNumber;
+//     }
 
-    public String getTitle() {
-        return title;
-    }
+//     public String getTitle() {
+//         return title;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public void setContractNumber(String contractNumber) {
-        this.contractNumber = contractNumber;
-    }
+//     public void setContractNumber(String contractNumber) {
+//         this.contractNumber = contractNumber;
+//     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-}
+//     public void setTitle(String title) {
+//         this.title = title;
+//     }
+// }
+
