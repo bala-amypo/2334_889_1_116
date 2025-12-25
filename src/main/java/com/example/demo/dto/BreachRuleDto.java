9f5bcb9c5e8bd4a667b7c6cb8d9b1f4137a7d463
bala-amypo <bala@amypo.in>
2025-12-25 10:30:46 +0000

    User commit

--- src/main/java/com/example/demo/dto/BreachRuleDto.java
+++ src/main/java/com/example/demo/dto/BreachRuleDto.java
@@ -9,52 +9,8 @@ public class BreachRuleDto {
     private Double maxPenaltyPercentage;
     private Boolean isDefaultRule;
 
-    public BreachRuleDto() {}
-
-    public String getRuleName() {
-        return ruleName;
+    public BreachRuleDto() {
     }
-
-    public void setRuleName(String ruleName) {
-        this.ruleName = ruleName;
-    }
-
-    public BigDecimal getPenaltyPerDay() {
-        return penaltyPerDay;
-    }
-
-    public void setPenaltyPerDay(BigDecimal penaltyPerDay) {
-        this.penaltyPerDay = penaltyPerDay;
-    }
-
-    public Double getMaxPenaltyPercentage() {
-        return maxPenaltyPercentage;
-    }
-
-    public void setMaxPenaltyPercentage(Double maxPenaltyPercentage) {
-        this.maxPenaltyPercentage = maxPenaltyPercentage;
-    }
-
-    public Boolean getIsDefaultRule() {
-        return isDefaultRule;
-    }
-
-    public void setIsDefaultRule(Boolean isDefaultRule) {
-        this.isDefaultRule = isDefaultRule;
-    }
-}
-package com.example.demo.dto;
-
-import java.math.BigDecimal;
-
-public class BreachRuleDto {
-
-    private String ruleName;
-    private BigDecimal penaltyPerDay;
-    private Double maxPenaltyPercentage;
-    private Boolean isDefaultRule;
-
-    public BreachRuleDto() {}
 
     public String getRuleName() {
         return ruleName;
