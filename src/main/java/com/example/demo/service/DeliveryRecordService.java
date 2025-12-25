5b6442ad7174709bf93834b57acbcfdac4f5b9ac
bala-amypo <bala@amypo.in>
2025-12-25 07:47:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DeliveryRecordService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.DeliveryRecord;
+import java.util.List;
+
+public interface DeliveryRecordService {
+
+    DeliveryRecord createDeliveryRecord(DeliveryRecord record);
+
+    DeliveryRecord getLatestDeliveryRecord(Long contractId);
+
+    DeliveryRecord getRecordById(Long id);
+
+    List<DeliveryRecord> getDeliveryRecordsForContract(Long contractId);
+}
