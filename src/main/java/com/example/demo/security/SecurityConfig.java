d9184b12212d6f4f25c5a30bcd18e2b9b11a016c
bala-amypo <bala@amypo.in>
2025-12-27 15:41:15 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,32 +1,32 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import com.example.demo.security.JwtAuthenticationEntryPoint;
-// import com.example.demo.security.JwtAuthenticationFilter;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import com.example.demo.security.JwtAuthenticationEntryPoint;
+import com.example.demo.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// public class SecurityConfig {
+@Configuration
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-//         http
-//             .csrf(csrf -> csrf.disable())
-//             .authorizeHttpRequests(auth -> auth
-//                 .requestMatchers(
-//                         "/swagger-ui/**",
-//                         "/v3/api-docs/**",
-//                         "/auth/**"
-//                 ).permitAll()
-//                 .anyRequest().permitAll()
-//             )
-//             .exceptionHandling(ex ->
-//                 ex.authenticationEntryPoint(new JwtAuthenticationEntryPoint())
-//             );
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/auth/**"
+                ).permitAll()
+                .anyRequest().permitAll()
+            )
+            .exceptionHandling(ex ->
+                ex.authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+            );
 
-//         return http.build();
-//     }
-// }
+        return http.build();
+    }
+}
