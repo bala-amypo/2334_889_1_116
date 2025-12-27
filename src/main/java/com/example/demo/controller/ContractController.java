4cfbd785148e838ce434c9956a35f258869124dd
bala-amypo <bala@amypo.in>
2025-12-27 06:36:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/ContractController.java
+++ src/main/java/com/example/demo/controller/ContractController.java
@@ -1,39 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.entity.Contract;
-// import com.example.demo.service.ContractService;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/contracts")
-// public class ContractController {
-
-//     ContractService contractService;
-
-//     @PostMapping
-//     public Contract create(@RequestBody Contract contract) {
-//         return contractService.createContract(contract);
-//     }
-
-//     @GetMapping("/{id}")
-//     public Contract get(@PathVariable Long id) {
-//         return contractService.getContractById(id);
-//     }
-
-//     @GetMapping
-//     public List<Contract> list() {
-//         return contractService.getAllContracts();
-//     }
-// }
-
-
-
-
-
-
-
 
 package com.example.demo.controller;
 
