dec671688eabea3e93e8dd3b164faa39a76a2235
bala-amypo <bala@amypo.in>
2025-12-27 05:01:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -7,28 +7,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Contract {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String contractNumber;
-    private String title;
-    private String counterpartyName;
-    private LocalDate agreedDeliveryDate;
-    private BigDecimal baseContractValue;
-    private String status = "ACTIVE";
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-}
-
 // @Entity
 // @Getter @Setter
 // @NoArgsConstructor
@@ -40,27 +18,49 @@ public class Contract {
 //     @GeneratedValue(strategy = GenerationType.IDENTITY)
 //     private Long id;
 
-//     @Column(unique = true, nullable = false)
 //     private String contractNumber;
-
-//     @NotBlank
 //     private String title;
-
-//     @NotBlank
 //     private String counterpartyName;
-
-//     @NotNull
 //     private LocalDate agreedDeliveryDate;
-
-//     @NotNull
 //     private BigDecimal baseContractValue;
-
 //     private String status = "ACTIVE";
 
-//     @CreationTimestamp
-//     @Column(updatable = false)
 //     private LocalDateTime createdAt;
-
-//     @UpdateTimestamp
 //     private LocalDateTime updatedAt;
 // }
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String contractNumber;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String counterpartyName;
+
+    @NotNull
+    private LocalDate agreedDeliveryDate;
+
+    @NotNull
+    private BigDecimal baseContractValue;
+
+    private String status = "ACTIVE";
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
